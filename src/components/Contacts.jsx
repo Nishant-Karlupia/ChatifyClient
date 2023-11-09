879deb5cd8e20666e5876e3fa92ffbc28dc270d7
Nishant Karlupia <nishantkarlupia149@gmail.com>
2023-11-09 14:05:17 +0530

    icons changed

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import styled from 'styled-components'
-import Logo from "../assets/logo1.svg";
+import Logo from "../assets/logo.svg";
 
 const Contacts = ({contacts,currentUser,changeChat}) => {
 
